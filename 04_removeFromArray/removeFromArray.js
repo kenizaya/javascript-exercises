12275fae59a48777b65d761b87194e2d42c0209f
kenizaya <harveylstark@gmail.com>
2022-05-07 23:02:01 +0530

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    let args = [...arr];
+    let index;
+
+    for (let i = 0; i < theArgs.length; i++) {
+        index = args.indexOf(theArgs[i]);
+        if (index !== -1) {
+            args.splice(index, 1);
+        }
+        
+    }
+
+    return args;
 
 };
 
