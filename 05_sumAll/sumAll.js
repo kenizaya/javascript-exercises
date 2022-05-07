cb3d7476b0819bc815a33a10366b75035994f438
kenizaya <harveylstark@gmail.com>
2022-05-07 23:12:22 +0530

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    let temp;
+
+    if (typeof start != "number" || typeof end != "number" ||
+        start < 0 || end < 0) {
+        return "ERROR"
+    }
+
+    if (start > end) {
+        temp = start;
+        start = end;
+        end = temp;
+    }
+
+
+    result = 0;
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 
 };
 
