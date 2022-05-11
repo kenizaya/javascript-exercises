eeb4996f5c4f77d41831080b7f02c20509ab9bc9
kenizaya <harveylstark@gmail.com>
2022-05-11 23:01:04 +0530

    Remove unnecessary logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,7 @@
 const palindromes = function (s) {
     s =  s.replace(/\W|_/g, '').toLowerCase();
-    const half = Math.floor(s.length/2);
-    const arr = s.split("", half);
 
-    return arr.reverse().join("") === ((s.length % 2 === 0) ?
-        s.substring(half) : s.substring(half + 1));
+    return s.split("").reverse().join("") === s;
 };
 
 // Do not edit below this line
