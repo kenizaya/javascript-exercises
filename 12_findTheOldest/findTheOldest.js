ba8c1edff44bbadbe3c1a1ab7c223705a758a94a
kenizaya <harveylstark@gmail.com>
2022-05-12 00:01:55 +0530

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    
+    let index;
+    let age = [];
 
+    obj.map(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        let a = person.yearOfDeath - person.yearOfBirth;
+        age.push(a);
+    });
+    index = age.indexOf(Math.max(...age));
+
+    return obj[index];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
