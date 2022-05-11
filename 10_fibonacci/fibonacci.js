eae7fcd1721c01c51a4bac2fe1e3c074112cae2d
kenizaya <harveylstark@gmail.com>
2022-05-11 23:34:13 +0530

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let prev = 0;
+    let current = 1;
+    let fib = 1;
+
+    if (n < 0) {return "OOPS";}
+    
+    for (let i = 1; i < n; i++) {
+        fib = prev + current;
+        prev = current;
+        current = fib;
+    }
+    
+    return fib;
 };
 
 // Do not edit below this line
