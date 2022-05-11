39f1c9d0214b040da093c28c35d572a7078b1273
kenizaya <harveylstark@gmail.com>
2022-05-11 22:57:58 +0530

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s =  s.replace(/\W|_/g, '').toLowerCase();
+    const half = Math.floor(s.length/2);
+    const arr = s.split("", half);
 
+    return arr.reverse().join("") === ((s.length % 2 === 0) ?
+        s.substring(half) : s.substring(half + 1));
 };
 
 // Do not edit below this line
