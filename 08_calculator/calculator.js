9f8bafccebe81e8526a201ace6d07ee10d6a3929
kenizaya <harveylstark@gmail.com>
2022-05-11 18:58:20 +0530

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((a, b) => a + b, 0);
 	
 };
 
-const sum = function() {
-	
-};
+const multiply = function(args) {
 
-const multiply = function() {
+  return args.reduce((a, b) => a * b);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let total = 1;
+
+  while (n > 1) {
+    total *= n;
+    n--;
+  }
 	
+  return total;
 };
 
 // Do not edit below this line
